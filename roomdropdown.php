ace716b074203de7d686425cfbdf1156846cf2cb
Brian Gossett <84483732+BrianGossett@users.noreply.github.com>
2024-02-12 17:32:26 -0600

    Fixed some stuff

--- roomdropdown.php
+++ roomdropdown.php
@@ -3,20 +3,20 @@
 include 'db_connection.php';
 
 // Query to select rooms from the database
-$query = "SELECT RoomID, RoomName FROM room";
+$query = "SELECT RoomID, RoomSize FROM roomsize";
 $result = $conn->query($query);
 
 // Check if query was successful
 if ($result) {
     // Start building the dropdown menu
-    $dropdown = '<select id="RoomID">'
+    $dropdown = '<select id="RoomID">';
 
     // Loop through the retrieved rooms
     while ($row = $result->fetch_assoc()) {
         $roomId = $row['RoomID'];
-        $roomName = $row['RoomName'];
+        $roomSize = $row['RoomSize'];
         // Add an option for each room
-        $dropdown .= "<option value='$roomId'>$roomName</option>";
+        $dropdown .= "<option value='$roomId'>$roomSize</option>";
     }
 
     // Close the select tag
